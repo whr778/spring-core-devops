7091daf7d5431dd14631ab5bd1980ee62c256e06
whr778 <whr778@gmail.com>
2017-03-03 12:06:50 -0500

    Update SpringCoreDevOpsApplication.java

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCoreDevOpsApplication {
 
+	/**
+	*
+	**/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
